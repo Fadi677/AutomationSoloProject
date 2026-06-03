ef3df1c85eedf52a1f7eb09d9e53d9ac6b34d529
Fadi Abuaita <fadimabuaita@gmail.com>
2026-06-03 17:45:11 +0300

    project folder structure

---
+++ src/main/java/solo/fadi/imdb_automation_project/ImdbPage.java
@@ -0,0 +1,6 @@
+package solo.fadi.imdb_automation_project;
+
+
+public class ImdbPage {
+    
+}
---
+++ src/test/java/solo/fadi/imdb_automation_project/ImdbTests.java
@@ -0,0 +1,6 @@
+package solo.fadi.imdb_automation_project;
+
+public class ImdbTests extends BaseTest {
+
+
+}
